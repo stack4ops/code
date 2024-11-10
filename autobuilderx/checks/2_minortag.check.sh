08ff6a50bac8abeaa3cd5081d8bc486c5ab1bea9
Stefan Schneider <eqsoft4@gmail.com>
2024-11-10 11:55:00 +0100

    add --tls-verify to skopeo for local registry checks

--- autobuilderx/checks/2_minortag.check.sh
+++ autobuilderx/checks/2_minortag.check.sh
@@ -3,6 +3,11 @@ if [ "${pipeline_env}" = "local" ] && [ "${local_only}" = "1" ]; then
   return 18
 fi
 
+tls_verify=''
+if [ "${insecure_tls:?}" = "1" ]; then
+    tls_verify="--tls-verify=false"
+fi
+
 creds=""
 
 if [ -n "${base_registry_user:-}" ] && [ -n "${base_registry_pass:-}" ]; then
@@ -17,14 +22,14 @@ get_minor_tag() {
     return 0
   fi
   
-  if ! repository_tags=$(skopeo inspect $creds "docker://$base_image:$base_tag"); then
+  if ! repository_tags=$(skopeo inspect "${tls_verify}" $creds "docker://$base_image:$base_tag"); then
     log 3 "something went wrong while inspecting the image docker://$base_image:$base_tag"
     return 1
   fi
 
   log 5 "minor_tag_regex: ${minor_tag_regex}"
   
-  current_digest=$(skopeo inspect $creds --no-tags --format '{{ .Layers }}' docker://${base_image}:${base_tag} | tr -d '"' | tr -d '[:space:]')
+  current_digest=$(skopeo inspect "${tls_verify}" $creds --no-tags --format '{{ .Layers }}' docker://${base_image}:${base_tag} | tr -d '"' | tr -d '[:space:]')
 
   log 5 "current_digest: ${current_digest}"
 
@@ -34,7 +39,7 @@ get_minor_tag() {
   fi
   for minor_tag in $minor_tags; do
     mt=$(echo $minor_tag | tr -d '"')
-    if ! minor_tag_digest=$(skopeo inspect $creds --no-tags --format '{{ .Layers }}' docker://${base_image}:$mt | tr -d '"' | tr -d '[:space:]'); then
+    if ! minor_tag_digest=$(skopeo inspect "${tls_verify}" $creds --no-tags --format '{{ .Layers }}' docker://${base_image}:$mt | tr -d '"' | tr -d '[:space:]'); then
       log 3 "something went wrong while getting the minor_tag_digest for docker://${base_image}:$mt"
     fi
     log 5 "$minor_tag_digest - $mt"
