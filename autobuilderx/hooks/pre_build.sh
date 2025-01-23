76d6e9d9963866075a5ec276a3869c86ea6e0318
Stefan Schneider <eqsoft4@gmail.com>
2025-01-23 09:55:05 +0100

    delete autobuilderx/hooks/pre_build.sh

--- autobuilderx/hooks/pre_build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-if [ "${pipeline_env:?}" = 'local' ]; then
-  docker exec buildx_buildkit_cibuilder0 /bin/sh -c "echo '172.25.3.202 registry.hrz.uni-marburg.de' >> /etc/hosts"
-  docker exec buildx_buildkit_cibuilder0 /bin/sh -c "ping -c 3 registry.hrz.uni-marburg.de"
-fi
