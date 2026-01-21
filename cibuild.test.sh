afc00c5e6c3307701313ae6cc88324bae9e643fe
Stefan Schneider <eqsoft4@gmail.com>
2026-01-21 09:11:41 +0100

    initial refactoring

--- cibuild.test.sh
+++ cibuild.test.sh
@@ -1,12 +1,17 @@
 #!/bin/sh
+cibuild_log_info "not implemented yet"
 
-ret=$(docker run -d --rm --name $container $test_image)
+# ls -la /home/cibuilder/
+# if ! docker run -d --rm --name $container $test_image; then
+#   log 0 "[failed] Test failed!"
+#   exit 1
+# fi
 
-sleep 5
+# sleep 5
 
-if ! docker logs $container 2>&1 | grep "Web UI available at"; then
-  log 0 "[failed] Test failed!"
-  exit 1
-fi
+# if ! docker logs $container 2>&1 | grep "Web UI available at"; then
+#   log 0 "[failed] Test failed!"
+#   exit 1
+# fi
 
-log 1 "[success] Test successful"
+# log 1 "[success] Test successful"
